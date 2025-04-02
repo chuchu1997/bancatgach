ce9822689fb0c73b9a81aa8d7fa2683fb5410d13
cuong <cuong@foxit.co.jp>
2025-04-02 11:35:51 +0700

    edit phone number

--- src/components/layouts/BlockPhoneNumber.tsx
+++ src/components/layouts/BlockPhoneNumber.tsx
@@ -9,7 +9,7 @@ const BlockPhoneNumber = () => {
         width={35}
         height={35}
         className="group-hover:scale-125 scale-100 transition-all duration-400"></img>
-      <span className="text-white font-semibold "> 0862.398.558</span>
+      <span className="text-white font-semibold "> 0973 926 139</span>
     </div>
   );
 };
